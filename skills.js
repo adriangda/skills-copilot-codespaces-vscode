c505b24d8558bb06cbd07b62e1be1139945721d9
adriangda <142631268+adriangda@users.noreply.github.com>
2024-03-14 19:39:32 +0000

    typo fix in js function

--- skills.js
+++ skills.js
@@ -1,4 +1,4 @@
-function CalculateNumbers() {
+function calculateNumbers() {
     var number1 = parseInt(document.getElementById('number1').value);
     var number2 = parseInt(document.getElementById('number2').value);
     var result = number1 + number2;
